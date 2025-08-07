42234dc0d453cfb872c8093a99fd02e9a0bb0cc4
Zain Naqvi <zainnaqvi.200621@gmail.com>
2025-08-07 15:04:33 -0400

    Update simulation.ino

--- simulation.ino
+++ simulation.ino
@@ -1,1 +1,4 @@
+//This file is responsible for faking data from a DVL
+//We are using a Nortek Nucleus 1000 as our DVL
+// https://support.nortekgroup.com/hc/en-us/article_attachments/17223428270620
 
