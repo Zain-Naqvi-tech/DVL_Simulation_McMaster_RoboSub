8e277b13cfeb69fcaaa5ff7f224c18129ba19ec8
Zain Naqvi <zainnaqvi.200621@gmail.com>
2025-08-08 17:53:02 -0400

    Update DVL.cpp

--- DVL.cpp
+++ DVL.cpp
@@ -9,3 +9,36 @@ DVL_SIM::DVL_SIM(IPAddress ip, unsigned int port){
   vx = vy = vz = 0.0f;
   d1 = d2 = d3 = d4 = 5.0f; //just for basic initialisation
 }
+
+//Basic start for ethernet communication - sets up the DVL to send udp packets to the teensy
+void DVL_SIM::begin(){
+  Ethernet.begin(teensy);
+  udp.begin(updPort);
+}
+
+void DVL_SIM::init(){
+  vx = randomVelocity();
+  vy = randomVelocity();
+  vz = randomVelocity();
+
+  d1 = randomDistance();
+  d2 = randomDistance();
+  d3 = randomDistance();
+  d4 = randomDistance();
+
+  String final_text = finalSentence();
+
+  //Send the udp packets (figuring out how to code this right now) (All the above data can be sent through udp packets to simulate a dvl)
+
+  
+}
+
+//Delegation Starts
+//Zain
+void DVL_SIM::finalSentence(){
+  string s = "";
+  //we can concatenate everythig to this string
+  s += "
+}
+
+
