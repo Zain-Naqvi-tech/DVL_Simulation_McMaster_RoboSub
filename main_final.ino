7418992df18621ddb3ab975614f9c2b1ad6116fd
Zain Naqvi <zainnaqvi.200621@gmail.com>
2025-08-12 16:47:51 -0400

    Update main_final.ino

--- main_final.ino
+++ main_final.ino
@@ -1,1 +1,12 @@
 
+#include <DVL_SIM.h> //import our library header
+
+IPAdress teensy(1,2,3,4); //pre-built IP for testing purposes (explaination in main.ino file)
+unsigned int udpPort = 12345;
+
+DVL_SIM dvl(teensy, updPort); //create an instance of the class
+
+void setup() {
+  Serial.begin(9600);
+  
+}
